7576bb8d670da570f579b1eaf7e7567b20f2c63e
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-01-14 13:05:02 +0100

    adding exception testing

---
+++ tests/test_exceptions.py
@@ -0,0 +1,25 @@
+import pytest
+from under_test.functions import divide_small_numbers
+
+
+def test_divide_small_numbers_exception():
+
+    with pytest.raises(Exception):
+        divide_small_numbers(101, 1)
+
+
+def test_divide_small_numbers_exceptions():
+
+    with pytest.raises(ValueError):
+        divide_small_numbers(101, 1)
+
+    with pytest.raises(ZeroDivisionError):
+        divide_small_numbers(0, 0)
+
+
+def test_divide_small_numbers_exception_content():
+
+    with pytest.raises(ValueError) as err:
+        divide_small_numbers(101, 1)
+
+    assert "Not accepting numbers over 100" in err.value.__str__()
--- under_test/functions.py
+++ under_test/functions.py
@@ -1,2 +1,8 @@
 def multiply(a: int, b: int) -> int:
     return a * b
+
+
+def divide_small_numbers(a: int, b: int) -> int:
+    if a > 100 or b > 100:
+        raise ValueError("Not accepting numbers over 100")
+    return a / b
